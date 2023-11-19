f0356981a305ba82e4d90cf93bebe320dd3a3564
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-11-19 02:56:09 -0900

    Clerk Auth Complete

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,10 @@
+
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex items-center justify-center h-full" >
+        {children}
+    </div>
+  );
+};
+
+export default ClerkLayout
