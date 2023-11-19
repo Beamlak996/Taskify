eb7de1432954a0e4867aa61f483c374e549b0bf6
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-11-19 02:33:02 -0900

    Config Complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,22 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { siteConfig } from '@/config/site'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Taskify',
-  description: 'Taskify',
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`
+  },
+  description: siteConfig.description,
+  icons: [
+    {
+      url: "/logo.svg",
+      href: "/logo.svg"
+    }
+  ]
 }
 
 export default function RootLayout({
