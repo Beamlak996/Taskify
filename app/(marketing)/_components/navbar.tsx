29586e3811b0d37c2bcde6c8969f65294d8dc405
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-11-19 22:20:16 -0900

    Desktop Sidebar Complete

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link"
 export const Navbar = () => {
   return (
     <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+      
       <div className="md:max-w-screen mx-auto flex items-center justify-between w-full">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
---
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import { Accordion } from "@/components/ui/accordion"
+import { Button } from "@/components/ui/button"
+import { Skeleton } from "@/components/ui/skeleton"
+import { useOrganization, useOrganizationList } from "@clerk/nextjs"
+import { Plus } from "lucide-react"
+import Link from "next/link"
+import { useLocalStorage } from "usehooks-ts"
+import { NavItem, Organization } from "./nav-item";
+
+type SidebarProps = {
+    storageKey?: string
+}
+
+const Sidebar = ({storageKey="t-sidebar-state"}: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(storageKey, {})
+
+  const {
+    organization: activeOrganization,
+    isLoaded: isLoadedOrg
+  } = useOrganization()
+
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+
+      return acc;
+    },
+    []
+  );
+
+  const onExpand = (id: string)=> {
+    setExpanded((curr)=> ({
+      ...curr,
+      [id]: !expanded[id]
+    }))
+  }
+
+  if(!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <div className="font-medium text-xs flex items-center mb-1 ">
+        <span className="pl-4">Workspaces</span>
+        <Button
+          type="button"
+          size="icon"
+          variant="ghost"
+          className="ml-auto"
+          asChild
+        >
+          <Link href="/select-org">
+            <Plus className="" />
+          </Link>
+        </Button>
+      </div>
+      <Accordion
+        type="multiple"
+        defaultValue={defaultAccordionValue}
+        className="space-y-2"
+      >
+        {userMemberships.data.map(({ organization }) => (
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            organization={organization as Organization}
+            onExpand={onExpand}
+          />
+        ))}
+      </Accordion>
+    </>
+  );
+}
+
+export default Sidebar
