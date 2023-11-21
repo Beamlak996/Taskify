9bbb6dd4ded212a02a16967d219bb35735ea9c5a
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-11-21 00:36:00 -0900

    useAction Complete

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,33 @@
+import { create } from "@/actions/create-board"
+import { Button } from "@/components/ui/button"
+import { db } from "@/lib/db"
 
-const OrganizarionIdPage = () => {
+const OrganizarionIdPage = async () => {
+  const boards = await db.board.findMany()
+
   return (
-    <div>
-        Organization  Page
+    <div className="flex flex-col space-y-4" >
+        <form action={create} >
+          <input 
+            name="title"
+            id="title"
+            required
+            placeholder="Enter a board name"
+            className="border-black border p-1"
+          />
+          <Button type="submit" >
+            Submit
+          </Button>
+        </form>
+        <div className="space-y-2" >
+          {
+            boards.map((board)=> (
+              <div key={board.id} >
+                Board name: {board.title}
+              </div>
+            ))
+          }
+        </div>
     </div>
   )
 }
