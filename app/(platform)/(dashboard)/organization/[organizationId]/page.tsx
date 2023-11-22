c1e732e44d000a4650601ac85d73e52c4d99e9e6
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-11-22 07:41:31 -0900

    Board List Complete

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,18 +1,21 @@
 import { Separator } from "@/components/ui/separator"
 import { Info } from "./_components/info"
 import { BoardList } from "./_components/board-list"
+import { Suspense } from "react"
 
 const OrganizarionIdPage = async () => {
 
   return (
-    <div className="w-full mb-20" >
-        <Info />
-        <Separator className="my-4" />
-        <div className="px-2 md:px-4" >
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />} >
           <BoardList />
-        </div>
+        </Suspense>
+      </div>
     </div>
-  )
+  );
 }
 
 export default OrganizarionIdPage
