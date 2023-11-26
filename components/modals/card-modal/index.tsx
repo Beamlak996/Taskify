aa397d13cd9c33909588fd643282e44512140851
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-11-26 12:33:27 -0900

    Card Actions Complete

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -6,6 +6,8 @@ import { fetcher } from "@/lib/fetcher"
 import { CardWithList } from "@/types"
 import { useQuery } from "@tanstack/react-query"
 import { Header } from "./header"
+import { Description } from "./description"
+import { Actions } from "./actions"
 
 export const CardModal = ()=> {
     const cardModal = useCardModal()
@@ -19,6 +21,26 @@ export const CardModal = ()=> {
       <Dialog open={cardModal.isOpen} onOpenChange={cardModal.onClose}>
         <DialogContent>
           {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+          <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4" >
+            <div className="col-span-3" >
+              <div className="w-full space-y-6" >
+                {
+                  !cardData ? (
+                    <Description.Skeleton />
+                  ) : (
+                    <Description data={cardData} />
+                  ) 
+                }
+              </div>
+            </div>  
+            {
+              !cardData ? (
+                <Actions.Skeleton />
+              ) : (
+                <Actions data={cardData} />
+              ) 
+            }
+          </div>
         </DialogContent>
       </Dialog>
     );
